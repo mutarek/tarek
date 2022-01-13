1974a86b8f2240bfe8e99d1d2b258f087a6d5e14
M Tarek <mohiuddintarek2017@gmail.com>
2022-01-13 19:26:38 +0600

    updated

--- home.php
+++ home.php
@@ -4,13 +4,35 @@
 	<title>Dhaka City</title>
 </head>
 <body>
+<?php
+//Cast float to int
+$floatvalue = 12.329909;
+$intvalue = (int)$floatvalue;
+echo $intvalue;
+echo "</br>";
+// cast string to int 
+$sringvalue = "12323.43";
+$Intvalue = (int)$sringvalue;
+echo $Intvalue;
+// to find absoulute value 
+echo "</br>";
+$rawvalue =-6.7;
+echo abs($rawvalue);
+echo "</br>";
+//to find a squre root
+$totalsqure = 64;
+echo sqrt($totalsqure);
+echo "</br>";
+//to round a digit 
+$assignvalue = 0.60;
+$nonassignvalue = 0.49;
+echo round($assignvalue);
+echo "</br>";
+echo round($nonassignvalue);
 
-	<?php
 
-$value = 10.0;
-var_dump($x);
 
-	?>
+?>
 
 <h1>Hello Dhaka CIty</h1>
 
