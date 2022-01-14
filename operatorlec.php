2772fcddf1899709dca7f5168a9ea38584520b72
M Tarek <mohiuddintarek2017@gmail.com>
2022-01-14 21:50:36 +0600

    operatorlec

---
+++ operatorlec.php
@@ -0,0 +1,21 @@
+<?php
+$num1 = 5;
+$num2 = 5;
+
+//Addition
+#echo $num1 + $num2;
+//Subtraction
+#echo $num1 - $num2;
+
+//Multiplication
+#echo $num1 * $num2;
+// Division
+#echo $num1 /$num2;
+//Modulus
+#echo $num1 % $num2;
+
+//Exponentiation
+#echo $num1 ** $num2;
+
+
+?>
